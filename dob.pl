22d10a26ff9e58d35820a0f06931c6bf6020bb33
Bramhani Chowdary <bramhanichowdary2007@gmail.com>
2026-02-18 14:49:14 +0530

    Add files via upload

--- dob.pl
+++
@@ -1,5 +0,0 @@
-person(ram,'10-01-2000').
-person(sita,'11-04-2008').
-person(raju,'21-07-2002').
-
-
--- family tree.pl
+++
@@ -1,7 +0,0 @@
-parent(ram,raj).
-parent(rani,sunny).
-parent(Lenovo,HP).
-
-father(X,Y) :- parent(X,Y).
-grandparent(X,Z) :- parent(X,Y), parent(Y,Z).
-
